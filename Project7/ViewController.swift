71f69f29e89f2d5eeb07044e34432dc84c944038
Eddie Jung <ejung716@gmail.com>
2021-08-06 15:02:58 -0700

    Display DetailViewController with custom html body text when a row is selected.

--- Project7/ViewController.swift
+++ Project7/ViewController.swift
@@ -14,17 +14,16 @@ class ViewController: UITableViewController {
         super.viewDidLoad()
         
         let urlString = "https://www.hackingwithswift.com/samples/petitions-1.json"
-        
         if let url = URL(string: urlString) {
             if let data = try? Data(contentsOf: url) {
                 parse(json: data)
             }
         }
+        
     }
     
     func parse(json: Data) {
         let decoder = JSONDecoder()
-        
         if let jsonPetitions = try? decoder.decode(Petitions.self, from: json) {
             petitions = jsonPetitions.results
             tableView.reloadData()
@@ -32,15 +31,21 @@ class ViewController: UITableViewController {
     }
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return petitions.count
+        petitions.count
     }
-
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
         let petition = petitions[indexPath.row]
         cell.textLabel?.text = petition.title
         cell.detailTextLabel?.text = petition.body
         return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let vc = DetailViewController()
+        vc.detailItem = petitions[indexPath.row]
+        navigationController?.pushViewController(vc, animated: true)
     }
 
 }
