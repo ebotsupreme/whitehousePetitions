2d74e3d3abaa53961d610e7d57ed8d7708b361cc
Eddie Jung <ejung716@gmail.com>
2021-08-06 13:49:16 -0700

    initial commit. Add storyboard tableViewController, NavigationController, and tabBarController. Conform to UITableViewController protocol.

---
+++ Project7/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Project7
+//
+//  Created by Eddie Jung on 8/6/21.
+//
+
+import UIKit
+
+class ViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+    }
+
+
+}
+
