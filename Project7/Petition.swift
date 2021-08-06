f5b888204cd18f34bfb5b035150ae602a33a017b
Eddie Jung <ejung716@gmail.com>
2021-08-06 13:59:56 -0700

    Create Petition struct that conforms to Codable

---
+++ Project7/Petition.swift
@@ -0,0 +1,14 @@
+//
+//  Petition.swift
+//  Project7
+//
+//  Created by Eddie Jung on 8/6/21.
+//
+
+import Foundation
+
+struct Petition: Codable {
+    var title: String
+    var body: String
+    var signatureCount: Int
+}
